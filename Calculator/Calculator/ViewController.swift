438e6765468e6f11baba95a20e8768ec3ca3807b
Flint <flint@iMac1.local>
2019-03-08 09:19:29 -0500

    Created view of claculator

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Flint on 3/7/19.
+//  Copyright © 2019 Flint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
