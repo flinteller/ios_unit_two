4ade40e009bcb8923097ea5034f4616c5e0f7832
Flint <flint@iMac1.local>
2019-03-13 13:29:27 -0400

    Finished Apple pie hangman game

---
+++ Apple Pie/Apple Pie/Game.swift
@@ -0,0 +1,36 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Flint on 3/8/19.
+//  Copyright © 2019 Flint. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            }
+            else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+
+    }
+}
